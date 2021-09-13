f1230276c2177098b70f6736b02c611a20a862b2
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-14 00:45:31 +0900

    Add health check route

--- tutorial-express-nodejs-mongoose/source/server.ts
+++ tutorial-express-nodejs-mongoose/source/server.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logging from './config/logging';
 import config from './config/config';
+import sampleRoutes, { route } from './routes/sample';
 
 const NAMESPACE = 'Server';
 const router = express();
@@ -24,5 +25,34 @@ router.use((req, res, next) => {
 });
 
 /** Parse the request */
+// todo : @Deprecated, bodyParser will change
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+
+/** Rules of our API */
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // from anywhere
+  res.header('Access-Control-Allow-Headers', 'Origin, x-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method == 'OPTIONS') {
+    res.header('Access-Controll-Allow-Methods', 'GET PATCH DELETE POST PUT');
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+/** Routes */
+router.use('/sample', sampleRoutes);
+
+/** Error Handling */
+router.use((req, res, next) => {
+  const error = new Error('not found');
+  return res.status(404).json({ message: error.message });
+});
+
+/** Create the Server */
+const httpServer = http.createServer(router);
+httpServer.listen(config.server.port, () =>
+  logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.hostname}`)
+);
