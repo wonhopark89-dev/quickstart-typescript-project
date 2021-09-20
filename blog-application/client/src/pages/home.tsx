07e4158744058cabfc6d7737bbcb0a1048bf672c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-21 03:08:54 +0900

    Add header and homepage

--- blog-application/client/src/pages/home.tsx
+++ blog-application/client/src/pages/home.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Container } from 'reactstrap';
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
 import IPageProps from '../interfaces/page';
 
 const HomePage: React.FunctionComponent<IPageProps> = props => {
-    return (<p>Home Page</p>);
+    return (
+        <Container fluid className="p-0">
+            <Navigation />
+            <Header title={"A Nerdy Blog Website"} headline="Check out what people have to say!" />
+            <Container className={"mt-5"}>
+                Blog stuff here...
+            </Container>
+        </Container>);
 };
 
 export default HomePage;
