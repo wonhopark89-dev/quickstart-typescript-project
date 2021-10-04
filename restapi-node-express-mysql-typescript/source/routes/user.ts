2694b9ada24d9ed6258814166a056ddde7674c12
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-10-05 01:29:15 +0900

    (｡•́︿•̀｡) Add middleware for validate

--- restapi-node-express-mysql-typescript/source/routes/user.ts
+++ restapi-node-express-mysql-typescript/source/routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import controller from '../controllers/user';
+import extractJWT from '../middleware/extarctJWT';
 
 const router = express.Router();
 
-router.get('/validate', controller.validateToken);
+router.get('/validate', extractJWT, controller.validateToken);
 router.post('/register', controller.register);
 router.post('/login', controller.login);
 router.get('/get/all', controller.getAllusers);
